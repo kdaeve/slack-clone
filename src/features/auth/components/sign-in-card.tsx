50f105c6bdc398fec8d478e087100d2ad1c4d1ad
Yuntian Wu <triple_h@outlook.com>
2025-01-11 17:49:14 -0800

    Initial commit

---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,42 @@
+import { Card, CardHeader, CardDescription, CardContent, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+
+export const SignInCard = () => {
+  return (
+    <Card className="w-full h-full p-8">
+      <CardHeader className="px-0 pt-0">
+        <CardTitle>
+          Log in to continue
+        </CardTitle>
+        <CardDescription>
+          Use your email or another service to continue
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-5 px-0 pb-0">
+        <form className="space-y-2.5">
+          <Input 
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Email"
+            type="email"
+            required
+          />
+          <Input 
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Password"
+            type="password"
+            required
+          />
+          <Button type="submit" className="w-full" size="lg" disabled={false}>
+            Continue
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
+};
