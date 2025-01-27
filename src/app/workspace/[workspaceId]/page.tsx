e130529734b32b3ad4939df11576f6a2dfa2de32
Yuntian Wu <triple_h@outlook.com>
2025-01-26 19:44:20 -0800

    Implement sidebar component & ui

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,16 +1,8 @@
-"use client";
-
-
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-
 const WorkspaceIdPage = () => {
-  const workspaceId = useWorkspaceId();
-  const { data } = useGetWorkspace({ id: workspaceId });
 
   return (
     <div>
-      Data: {JSON.stringify(data)}
+      Workspace ID Page
     </div>
    );
 }
